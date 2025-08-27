1e2616b1d74b9acf023abac0c3f97d811d24442b
Igor-04 <153778010+Igor-04@users.noreply.github.com>
2025-08-27 12:44:19 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Casino_ton/',
-  build: { outDir: 'dist', target: 'esnext' },
-  server: { port: 3000, open: true }
-})
