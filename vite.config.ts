8f4c6198633091a19f8bc8345e873d7d3b496bb3
Igor-04 <153778010+Igor-04@users.noreply.github.com>
2025-08-27 13:54:31 +0300

    chore: ignore build artifacts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
-  export default defineConfig({
-    plugins: [react()],
+export default defineConfig({
+  base: '/Casino_ton/',
+  plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
@@ -51,7 +52,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
